44465b66259db58109cefe279795a3fbed7b91d0
William Alan Patterson <walanpatterson@gmail.com>
2022-07-26 14:49:38 -0400

    Setup up local docker runtime and SQL creation file, such that we now have a table of comments according to create comments up

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/TutorialEdge/go-rest-api-course/internal/db"
+)
 
 func Run() error {
 	fmt.Println("Starting our application")
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to DB")
+		return err
+	}
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("Failed to migrate database")
+		return err
+	}
+	fmt.Println("Successfully Pinged our DB")
 	return nil
 }
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/TutorialEdge/go-rest-api-course
 
 go 1.18
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
